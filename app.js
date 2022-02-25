9499d0463178115eb658a4f68e53c1c37c7195b3
Luke <lucaspedroso@alunos.utfpr.edu.br>
2022-02-25 17:49:53 -0300

    Level 3 - Hashing Passwords

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const ejs = require("ejs");
 const mongoose = require("mongoose");
 const { MongoTopologyClosedError } = require("mongodb");
 const app = express();
-const encrypt = require("mongoose-encryption");
+/* const encrypt = require("mongoose-encryption"); */
+const md5 = require("md5");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -19,8 +20,8 @@ const userSchema = new mongoose.Schema ({
     password: String
 });
 
-// Level 2 - Database Encryption
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
+/* // Level 2 - Database Encryption
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]}); */
 
 const User = new mongoose.model("User", userSchema);
 
@@ -40,7 +41,8 @@ app.post("/register", function(req, res){
     //Level 1 - Register Users with Username and Password
     const newUser = User({
         email: req.body.username,
-        password: req.body.password
+        // Level 3 - Hashing Passwords
+        password: md5(req.body.password)
     });
 
     newUser.save(function(err){
@@ -55,7 +57,8 @@ app.post("/register", function(req, res){
 
 app.post("/login", function(req, res){
     const username = req.body.username;
-    const password = req.body.password;
+    //Hashing Passwords
+    const password = md5(req.body.password);
 
     //Level 1
     User.findOne({email: username}, function(err, foundUser){
