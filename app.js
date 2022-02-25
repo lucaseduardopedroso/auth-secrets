b2ce4ff5dc2d7a80ba7c1c388559fb4b72c0e3f2
Luke <lucaspedroso@alunos.utfpr.edu.br>
2022-02-25 13:47:06 -0300

    Level 1 - Register Users with Username and Password

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 //jshint esversion:6
 const express = require("express");
 const ejs = require("ejs");
-
+const mongoose = require("mongoose");
+const { MongoTopologyClosedError } = require("mongodb");
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
+
+mongoose.connect("mongodb://localhost:27017/userDB");
+
+const userSchema = {
+    email: String,
+    password: String
+};
+
+const User = new mongoose.model("User", userSchema);
 
 app.get("/", function(req, res){
     res.render("home");
@@ -18,6 +28,41 @@ app.get("/login", function(req, res){
 
 app.get("/register", function(req, res){
     res.render("register");
+});
+
+app.post("/register", function(req, res){
+    //Level 1 - Register Users with Username and Password
+    const newUser = User({
+        email: req.body.username,
+        password: req.body.password
+    });
+
+    newUser.save(function(err){
+        if(!err){
+            //Only render the "secrets" page when the user is logged in
+            res.render("secrets");
+        } else {
+            console.log(err);
+        }
+    });
+});
+
+app.post("/login", function(req, res){
+    const username = req.body.username;
+    const password = req.body.password;
+
+    //Level 1
+    User.findOne({email: username}, function(err, foundUser){
+        if(err){
+            console.log(err);
+        } else {
+            if(foundUser){
+                if(foundUser.password === password){
+                    res.render("secrets");
+                }
+            }
+        }
+    });
 });
 
 app.listen(3000, function(){
